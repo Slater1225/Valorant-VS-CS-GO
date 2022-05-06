d8e4bb54580329b86bc5d4cbf1a61b01894a14ee
Slater1225 <104081133+Slater1225@users.noreply.github.com>
2022-05-06 17:06:02 +0800

    Add files via upload

---
+++ indexJS.js
@@ -0,0 +1,17 @@
+$("#but1").click(function(){
+  $("#cardCallout").html("<br>" + "What is CS:GO or Counter-strike: Global Offensive?" + "<br>" + "Counter-Strike: Global Offensive is a multiplayer first-person shooter developed by Valve and Hidden Path Entertainment. It is the fourth game in the Counter-Strike series. It has two teams competing in every round and the objective of every teams is to win the game." +
+      "The mode of the game is where the Terrorist team must plant and detonate a bomb in a specific location and defend it to be able to win the round and the Counter-Terrorist team must defuse the bomb when it is planted to be able to win the round. CS:GO is all about the teamwork, strategy, and gunplay. ");
+  $('body').css({background : 'url(csgo.jpeg)  no-repeat fixed center'});
+});
+
+$("#but2").click(function(){
+  $("#cardCallout").html("<br>" + "The comparison between the two games Valorant and CS:GO" + "<br>"
+    + "In Valorant the characters has a special ability and there are 4 categories controller, sentinel, initiator, and duelist. While in CS:GO there is not any besides Counter-terrist and Terrorist. The rounds of the game CS:GO has a 15 rounds to win the game meanwhile in Valorant has 13 rounds to finish the game. For the maps CS:GO has way larger maps than Valorant. Both games have a win-to-earn system. The more rounds you secure the money you earn. Both games need your full focus on the game and razor-sharp reflex. Both have a planter called the bomb but in the game Valorant it is named the Spike and both  are needed to be planted in the defending sites in order to win. The weapons on both games are very similar.");
+  $('body').css({background : 'url(valuu.jpg)  no-repeat fixed center'});
+});
+
+
+$("#but3").click(function(){
+  $("#cardCallout").html("<br>" + "What is Valorant?" + "<br>" + "Valorant is a free to play first-person shooter game developed and published by Riot Games. It is a 5v5 multiplayer game where one team attacks and the other team defends. In the beginning of the game, each player must select an agent or a character they want to use. The mode of the game is where the attacking team will plant a bomb called the spike and have it detonate and defend it in able to win the game and the other team will have to defend the sites and if the attacking team plants, they would have to defuse it in a certain time frame to be able to win the round or game. The game is all about the teamwork, strategy, and gunplay just like CSGO.");
+  $('body').css({background : 'url(valurant.jpg)  no-repeat fixed center'});
+});
